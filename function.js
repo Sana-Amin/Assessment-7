024e887183ca02f4a7d0df31eccb53b69ddb278d
Sana-Amin <amin.sana19@gmail.com>
2022-08-08 10:55:25 -0500

    Fixed

--- function.js
+++ function.js
@@ -1,18 +1,17 @@
-// function sumZero(array){
-//     let value = []
-// for (let i = 0; i < array.length; i++) {
-//   for (let j = 0; j < array.length; j++) {
-//     if (i !== j) {
-//       if (array[i] + array[j] === 0) {
-//         return true
-//       }
+function sumZero(array){  
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === 0) {
+        return true;
+      }
     
-//   }
-//   return false
-//  }
-// }
-// }
-// console.log(sumZero([]));
+ }
+}
+return false;
+}
+console.log(sumZero([]));
+//time Complexity is O(n^2)
+//space complexity is O(n)
 
 function hasUniqueChars(word) {
     let uniqueChars = new Set([])
@@ -25,4 +24,22 @@ console.log(hasUniqueChars("Monday"))
 console.log(hasUniqueChars("Moonday"))
 console.log(hasUniqueChars("Bob"))
 
-//run time is O(n)
+ //time Complexityis O(n)
+ //space complexity is O(n) because it based on the size of the word 
+
+ const isPangram = (string)=> new Set(string.toLowerCase().match(/[a-z]/g)).size === 26;
+
+
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
+
+//time Complexity is 0(n)
+//space complexity is O(n)
+
+const findLongestWord = (words) => {
+    return words.sort((a,b) => b.length - a.length)[0].length;
+};
+
+console.log(findLongestWord(["hi", "hello"]));
+//time Complexity is O(n log(n))
+//space complexity is O(n)
